84b738813d2fb52b9a8a61ad66dbc0e8a09911ed
David Ritchey <ritchey0713@gmail.com>
2018-03-29 01:30:40 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,8 +1,12 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count, 
+  attr_reader :title, :genre
   
   GENRES = []
+  
+  def genre=(genre)
+    @genre=genre
+
 
   def initialize(title)
     @title = title
